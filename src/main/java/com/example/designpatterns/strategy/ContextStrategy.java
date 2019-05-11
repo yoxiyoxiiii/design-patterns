e0bda1a735e9cf47331cf6f31995684498259e19
846814595@qq.com <846814595@qq.com>
2019-05-11 19:57:33 +0800

    策略模式

--- src/main/java/com/example/designpatterns/strategy/ContextStrategy.java
+++ src/main/java/com/example/designpatterns/strategy/ContextStrategy.java
@@ -1,5 +1,9 @@
 package com.example.designpatterns.strategy;
 
+/**
+ * 策略模式 是对算法（如都是完成支付算法）的封装，如支付算法，达到在算法直接的自由切换的目的。
+ * 同委派模式（命令模式）的根本区别是 命令模式 是封装的不同的算法（如登录和登出），通过命令来执行不同的算法。
+ */
 public class ContextStrategy implements PayStrategy{
     //持有策略应用，通过 Context 对象来切换各种策略
     private PayStrategy payStrategy;
